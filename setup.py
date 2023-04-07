1f941e161e1115c2cc426f8779aa752ccb56959e
Ted Johnson <tedjohnson12@g.ucla.edu>
2023-04-07 14:21:55 -0400

    make installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='pass_analyzer',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/tedjohnson12/wmata_pass_analyzer',
+    author='Ted Johnson',
+    author_email='tedjohnson12@g.ucla.edu',
+    license='GPL-3.0'
+)
